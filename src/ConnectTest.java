aabf43c261b30f202e73514143661697a1c65080
arajesh69 <send2rajesha@gmail.com>
2020-01-15 20:55:25 -0500

    New Class 'SuperTest' added

--- src/ConnectTest.java
+++ src/ConnectTest.java
@@ -3,9 +3,13 @@ public class ConnectTest {
     public static void main(String[] args) {
 
         System.out.println("test");
-        System.out.print("Git");
-        new AnotherFile();
+        System.out.println("Git");
 
+        AnotherFile myanother = new AnotherFile();
+        myanother.test();
+
+        SuperTest mysuper = new SuperTest();
+        mysuper.sample(3);
 
     }
 }
